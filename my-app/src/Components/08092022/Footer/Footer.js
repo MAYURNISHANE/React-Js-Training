c9f8a26ce62201f42f825a38827f6019f7120a3f
MAYURNISHANE <mayur.reflectiontp@gmail.com>
2023-07-05 13:36:08 +0530

    footer aaded

---
+++ my-app/src/Components/08092022/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='common'>
+            <div className='footer'>All copyright reserved @ Library of life</div>
+        </div>
+    )
+}
+
+export default Footer;
